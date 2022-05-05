83c6a2ba28eb4755a34d101006728d371faa9f52
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-05-06 00:47:02 +0600

    search inventory by id port is created in local host

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000
+const cors = require('cors')
+const port = process.env.PORT || 5050
+
+app.use(express.json())
+app.use(cors())
+
+
 
 const uri = process.env.URL
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -17,17 +23,19 @@ async function run(){
       const InventoryCollection = client.db('reven').collection('inventory');
 
       app.get('/', async(req, res) =>{
-         
-         
-          const size = 6
-
+         const size = 6
           const query = {};
           const cursor = InventoryCollection.find(query);
           const inventory = await cursor.limit(size).toArray();
-          
-          
-          res.send(inventory);
+         res.send(inventory);
       });
+
+      app.get('/inventory/:id', async(req, res) =>{
+        const id = req.params.id
+        const query = { "_id": ObjectId(id)};
+         const inventory = await InventoryCollection.findOne(query);
+        res.send(inventory);
+     });
 
      
 
